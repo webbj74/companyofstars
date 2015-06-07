141da404b44daeaea946c0365cb7225a6f3d33ba
Jonathan Webb <webbj74@users.noreply.github.com>
2015-06-07 19:24:16 -0400

    Updating build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -5,8 +5,11 @@ TARGET=$1
 CALLPATH=`dirname $0`
 HTTP_PORT=8080
 MYSQL_PORT=33066
-MYSQL_USER=admin
-MYSQL_PASS=admin123
+MYSQL_HOST=127.0.0.1
+MYSQL_USER=root
+MYSQL_PASS=root123
+DRUPAL_USER=admin
+DRUPAL_PASS=admin123
 
 # Remove target directory if it exists.
 if [[ -d "${TARGET}" ]]; then
@@ -18,6 +21,6 @@ drush make --working-copy --concurrency=5 "${CALLPATH}/${MAKEFILE}" "${TARGET}"
 
 if [[ -d "${TARGET}" ]]; then
   cd "${TARGET}"
-  drush si companyofstars --db-url=mysql://root:@127.0.0.1:${MYSQL_PORT}/drupal --account-name=${MYSQL_USER} --account-pass=${MYSQL_PASS} --account-mail=admin@example.com --site-mail=noreply@example.com --yes
+  drush si companyofstars --db-url=mysql://${MYSQL_USER}:${MYSQL_PASS}@${MYSQL_HOST}:${MYSQL_PORT}/drupal --account-name=${DRUPAL_USER} --account-pass=${DRUPAL_PASS} --account-mail=admin@example.com --site-mail=noreply@example.com --yes
 fi
 
