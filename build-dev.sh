e625d5866dd6fc741cfe9308fe53d15bb6dfc9c4
Jonathan Webb <webbj74@users.noreply.github.com>
2015-06-07 18:24:05 -0400

    parameterize mysql user/pass

--- build-dev.sh
+++ build-dev.sh
@@ -5,6 +5,8 @@ TARGET=$1
 CALLPATH=`dirname $0`
 HTTP_PORT=8080
 MYSQL_PORT=33066
+MYSQL_USER=admin
+MYSQL_PASS=admin123
 
 # Remove target directory if it exists.
 if [[ -d "${TARGET}" ]]; then
@@ -16,6 +18,6 @@ drush make --working-copy --concurrency=5 "${CALLPATH}/${MAKEFILE}" "${TARGET}"
 
 if [[ -d "${TARGET}" ]]; then
   cd "${TARGET}"
-  drush si companyofstars --db-url=mysql://root:@127.0.0.1:${MYSQL_PORT}/drupal --account-name=admin --account-pass=admin --account-mail=admin@example.com --site-mail=noreply@example.com --yes
+  drush si companyofstars --db-url=mysql://root:@127.0.0.1:${MYSQL_PORT}/drupal --account-name=${MYSQL_USER} --account-pass=${MYSQL_PASS} --account-mail=admin@example.com --site-mail=noreply@example.com --yes
 fi
 
